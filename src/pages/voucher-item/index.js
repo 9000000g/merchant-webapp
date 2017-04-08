2aa74a36f170266ebfc19ae30a7dd3db9d82e30e
nainemom <nainemom@gmail.com>
2017-04-08 17:08:35 +0430

    add voucher item page

---
+++ src/pages/voucher-item/index.js
@@ -0,0 +1,16 @@
+import template from './template.pug';
+import './style.css';
+export default {
+    template,
+    data () {
+        return {
+            title: 'کوپن شماره فلان'
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+}
