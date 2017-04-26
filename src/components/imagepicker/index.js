bc4039fc578fa6f340cbed12bdf55886c2ec48b4
nainemom <nainemom@gmail.com>
2017-04-26 18:47:12 +0430

    add imagepicker component

---
+++ src/components/imagepicker/index.js
@@ -0,0 +1,35 @@
+import template from './template.pug';
+import './style.css';
+export default {
+    template,
+    data(){
+        return {
+            file: null,
+            updateCount: 0
+        }
+    },
+    methods: {
+        fileChange(){
+            this.updateCount++;
+            //this.file = this.file;
+        }
+    },
+    props: {
+        pic: {
+            type: String,
+            default: 'https://www.dropbox.com/s/6dpq6av9wfeqoxo/flow.png?raw=1'
+        }
+    },
+    computed: {
+        thumb(){
+            const putUrl = (file)=>`url("${file}")`;
+            if( this.file !== null ){
+                return putUrl(URL.createObjectURL(this.file));
+                //return `'data:${this.file.type};base64,'${URL.createObjectURL(this.file)}`;
+            }
+            else{
+                return putUrl(this.pic);
+            }
+        }
+    }
+}
